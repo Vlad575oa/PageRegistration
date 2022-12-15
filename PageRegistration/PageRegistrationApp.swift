869efabdb9ab1242b75f4a2193cc2c9712e598b7
Vlad <vlad14196@gmail.com>
2022-12-15 14:32:16 +0300

    Initial Commit

---
+++ PageRegistration/PageRegistrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageRegistrationApp.swift
+//  PageRegistration
+//
+//  Created by user on 15.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct PageRegistrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
